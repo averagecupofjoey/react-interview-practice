f253faa0595a600269f505ab5b075af823df98a3
Joseph Elias <jrelias@outlook.com>
2022-04-22 13:26:55 -0400

    completed challenge 3

--- src/App.js
+++ src/App.js
@@ -1,25 +1,68 @@
-import logo from './logo.svg';
-import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <GenerateList />;
 }
+
+const GenerateList = () => {
+  //YOUR CODE HERE
+  const [activities, setActivities] = useState([]);
+
+  const generateActivity = function () {
+    const getActivity = async function () {
+      try {
+        let response = await axios.get('https://www.boredapi.com/api/activity');
+        console.log(response.data);
+        setActivities([...activities, response.data]);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+    getActivity();
+  };
+
+  useEffect(generateActivity, []);
+
+  return (
+    <>
+      <button onClick={generateActivity}>Get Activity</button>
+
+      {activities.length > 0 &&
+        activities.map((el) => {
+          return (
+            <>
+              <h1>{el.activity}</h1>
+              <ExpandableListItem item={el} />
+            </>
+          );
+        })}
+    </>
+  );
+};
+
+const ExpandableListItem = ({ item }) => {
+  // YOUR CODE HERE
+  const [isCollapsed, setIsCollapsed] = useState(true);
+
+  const toggleCollapse = function () {
+    setIsCollapsed(!isCollapsed);
+  };
+
+  return (
+    <>
+      <button onClick={toggleCollapse}>
+        {isCollapsed ? 'Expand' : 'Collapse'}
+      </button>
+
+      {!isCollapsed && (
+        <ul>
+          <li>type: {item.type}</li>
+          <li>participants: {item.participants}</li>
+        </ul>
+      )}
+    </>
+  );
+};
 
 export default App;
